7835367cb9921080053b31712411c2d9aa93b0ce
Erdene-Ochir <erdene-ochir@Erdene-Ochirs-MacBook-Air.local>
2020-06-26 08:48:42 +0800

    recurrent neural network

---
+++ RNN/rnn.py
@@ -0,0 +1,96 @@
+import torch
+from torch import nn
+import numpy as np 
+text = ['hey how are you','good i am fine','have a nice day']
+
+# Join all the sentences together and extract the unique characters from the combined sentences
+chars = set(''.join(text))
+
+# Creating a dictionary that maps integers to the characters
+int2char = dict(enumerate(chars))
+
+# Creating another dictionary that maps characters to integers
+char2int = {char: ind for ind, char in int2char.items()}
+
+# Finding the length of the longest string in our data
+maxlen = len(max(text, key=len))
+
+# Padding
+
+# A simple loop that loops through the list of sentences and adds a ' ' whitespace until the length of
+# the sentence matches the length of the longest sentence
+for i in range(len(text)):
+    while len(text[i])<maxlen:
+        text[i] += ' '
+
+# Creating lists that will hold our input and target sequences
+input_seq = []
+target_seq = []
+
+for i in range(len(text)):
+    # Remove last character for input sequence
+    input_seq.append(text[i][:-1])
+
+    # Remove first character for target sequence
+    target_seq.append(text[i][1:])
+    print("Input Sequence: {}\nTarget Sequence: {}".format(input_seq[i], target_seq[i]))
+
+for i in range(len(text)):
+    input_seq[i] = [char2int[character] for character in input_seq[i]]
+    target_seq[i] = [char2int[character] for character in target_seq[i]]
+
+dict_size = len(char2int)
+seq_len = maxlen - 1
+batch_size = len(text)
+
+def one_hot_encode(sequence, dict_size, seq_len, batch_size):
+    # Creating a multi-dimensional array of zeros with the desired output shape
+    features = np.zeros((batch_size, seq_len, dict_size), dtype=np.float32)
+    
+    # Replacing the 0 at the relevant character index with a 1 to represent that character
+    for i in range(batch_size):
+        for u in range(seq_len):
+            features[i, u, sequence[i][u]] = 1
+    return features
+
+input_seq = one_hot_encode(input_seq, dict_size, seq_len, batch_size)
+
+input_seq = torch.from_numpy(input_seq)
+target_seq = torch.Tensor(target_seq)
+
+is_cuda = torch.cuda.is_available()
+
+if is_cuda:
+    device = torch.device("cuda")
+    print("GPU is available")
+else:
+    device = torch.device("cpu")
+    print("GPU not available, CPU used")
+
+class Model(nn.Module):
+    def __init__(self,input_size, output_size, hidden_dim, n_layers):
+        super(Model, self).__init__()
+        self.hidden_dim = hidden_dim
+        self.n_layers = n_layers
+
+        #Defining the layers
+        # RNN Layer
+        self.rnn = nn.RNN(input_size, hidden_dim,n_layers,batch_first=True)
+        self.fc = nn.Linear(hidden_dim, output_size)
+    
+    def forward(self,x):
+        batch_size = x.size(0)
+        # Initializing hidden state for first input using method defined below
+        hidden = self.init_hidden(batch_size)
+        # Passing in the input and hidden state into the model and obtaining outputs
+        out, hidden = self.rnn(x, hidden)
+        # Reshaping the outputs such that it can be fit into the fully connected layer
+        out = out.contiguous().view(-1, self.hidden_dim)
+        out = self.fc(out)
+        return out, hidden
+
+    def init_hidden(self, batch_size):
+        # This method generates the first hidden state of zeros which we'll use in the forward pass
+        # We'll send the tensor holding the hidden state to the device we specified earlier as well
+        hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
+        return hidden
