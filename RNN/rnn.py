1996c6101da3a35585735b23d39447d447db6140
Erdene-Ochir <erdene-ochir@Erdene-Ochirs-MacBook-Air.local>
2020-06-26 08:58:31 +0800

    rnn

--- RNN/rnn.py
+++ RNN/rnn.py
@@ -60,6 +60,7 @@ target_seq = torch.Tensor(target_seq)
 
 is_cuda = torch.cuda.is_available()
 
+# If we have a GPU available, we'll set our device to GPU. We'll use this device variable later in our code.
 if is_cuda:
     device = torch.device("cuda")
     print("GPU is available")
@@ -94,3 +95,59 @@ class Model(nn.Module):
         # We'll send the tensor holding the hidden state to the device we specified earlier as well
         hidden = torch.zeros(self.n_layers, batch_size, self.hidden_dim)
         return hidden
+
+model = Model(input_size=dict_size, output_size=dict_size, hidden_dim=12, n_layers=1)
+# We'll also set the model to the device that we defined earlier (default is CPU)
+model.to(device)
+
+# Define hyperparameters
+n_epochs = 100
+lr=0.01
+
+# Define Loss, Optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+
+for epoch in range(1, n_epochs + 1):
+    print(epoch)
+    optimizer.zero_grad() # Clears existing gradients from previous epoch
+    input_seq.to(device)
+    output, hidden = model(input_seq)
+    loss = criterion(output, target_seq.view(-1).long())
+    loss.backward() # Does backpropagation and calculates gradients
+    optimizer.step() # Updates the weights accordingly
+    
+    if epoch%10 == 0:
+        print('Epoch: {}/{}.............'.format(epoch, n_epochs), end=' ')
+        print("Loss: {:.4f}".format(loss.item()))
+
+def predict(model, character):
+    # One-hot encoding our input to fit into the model
+    character = np.array([[char2int[c] for c in character]])
+    character = one_hot_encode(character, dict_size, character.shape[1], 1)
+    character = torch.from_numpy(character)
+    character.to(device)
+    
+    out, hidden = model(character)
+
+    prob = nn.functional.softmax(out[-1], dim=0).data
+    # Taking the class with the highest probability score from the output
+    char_ind = torch.max(prob, dim=0)[1].item()
+
+    return int2char[char_ind], hidden
+
+def sample(model, out_len, start='hey'):
+    model.eval() # eval mode
+    start = start.lower()
+    # First off, run through the starting characters
+    chars = [ch for ch in start]
+    size = out_len - len(chars)
+    # Now pass in the previous characters and get a new one
+    for ii in range(size):
+        char, h = predict(model, chars)
+        chars.append(char)
+
+    return ''.join(chars)
+
+print(sample(model, 15, 'good'))
